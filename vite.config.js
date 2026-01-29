e56fb380b1f89da54c23af1e4d21c2c451da8bf0
Moubarak Akamou <33613993+Makamou@users.noreply.github.com>
2026-01-29 05:02:46 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/chefChristian/",
   plugins: [react()],
+  base: './', // ✅ ensures assets load correctly in GitHub Pages or Vercel subpaths
 });
+
