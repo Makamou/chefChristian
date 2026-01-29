f82d43787e011506005590174a301dee14ff4f59
moubarak akamou <33613993+batman225@users.noreply.github.com>
2026-01-29 05:11:33 -0600

    Prepare for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ ensures assets load correctly in GitHub Pages or Vercel subpaths
+  base: "./", // ✅ relative paths fix 404s
 });
-
